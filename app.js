5cb914788e48e7befea280a477b73ee7636aa3e1
Nilesh Srivastava <nilesh.sri02@gmail.com>
2022-06-23 19:18:02 +0530

    Added basic functionality of the project.. Responsiveness and CSS are remaining

---
+++ app.js
@@ -0,0 +1,48 @@
+const { query } = require("express");
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req, res){
+
+    res.sendFile(__dirname+"/index.html");
+
+    
+});
+
+app.post("/", function(req,res){
+
+    const query = req.body.cityName;
+    const units= "metric";
+    const apiKey = "ee701323a47fd5290532bcdd900573ac";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+apiKey+"&units="+units;
+   
+   
+    https.get(url,function(response){
+
+
+        response.on("data",function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const desc = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
+            console.log(icon);
+
+            res.write("<h1>The current temp in "+ query +" is "+ temp +" degrees Celcius</h1>");
+            res.write("<p>The weather is currently " + desc +"</p>");
+            res.write("<img src="+ imageURL + ">");
+            res.send();
+        })
+
+    });
+})
+
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000")
+});
